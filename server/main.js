d896b8028529fef4fff6c20e4e3bb2a44189a31b
rhs-dnu <rhs@dnu.com.mx>
2024-05-07 09:44:51 -0600

    Primer commit clase 7 mayo

---
+++ server/main.js
@@ -0,0 +1,19 @@
+var express = require ('express');
+var app = express();
+
+/*como trabajaremos con socket, es recomendable usar el modulo HTTP para pasarle la app a express y manejar bien http*/
+var server = require('http').Server(app);
+
+/*aqui estara toda la funcionalidad de los sockets
+  se requiere la libreria socket.io
+  se pasa la variable server que tiene la app express y HTTP
+*/
+var io = require('socket.io')(server);
+
+appget('/', function(req, res){
+    res.status(200).send("Hola Mundo geis");
+});
+
+server.listen(3010, function(){
+    console.log("el server esta corriendo en http://localhost:3010")
+});
